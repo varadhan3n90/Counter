4e3d9cc17d575e411c9594446e4ceb6c27446d94
Varadhan <varadhan3n90@yahoo.co.in>
2013-01-02 15:39:04 +0530

    Some small changes to serial program for 8051

--- Counter/src/serialProgram/ButtonRead8051.java
+++ Counter/src/serialProgram/ButtonRead8051.java
@@ -2,57 +2,67 @@ package serialProgram;
 
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
+
 import java.io.IOException;
 import java.io.InputStream;
 
+
+/**
+ * This program uses the java RXTX library 2.71 and requires rxtxSerial.dll to be present in the java.library.path
+ * The program is supposed to be same for 64 bit but a different vendor provides support.
+ * @author Varadhan
+ *
+ */
 public class ButtonRead8051 {
-    void connect ( String portName ) throws Exception
-    {
+	
+	/**
+	 * 
+	 * @param portName example COM1, COM2
+	 * @throws NoSuchPortException 
+	 * @throws PortInUseException 
+	 * @throws UnsupportedCommOperationException 
+	 * @throws IOException 
+	 * @throws Exception
+	 */
+    void connect ( String portName ) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
+    	
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-        if ( portIdentifier.isCurrentlyOwned() )
-        {
+        if ( portIdentifier.isCurrentlyOwned() ){
             System.out.println("Error: Port is currently in use");
         }
-        else
-        {
-            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
-            
-            if ( commPort instanceof SerialPort )
-            {
+        else{            
+        	CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);            
+            if ( commPort instanceof SerialPort ){
                 SerialPort serialPort = (SerialPort) commPort;
-                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
-                
-                InputStream in = serialPort.getInputStream();
-                //OutputStream out = serialPort.getOutputStream();
-                while(true){				
-				byte[] bs = new byte[1];
-				in.read(bs);
-				String st = new String(bs).trim();
-				if(st!=null&&!st.equals(""))
-				System.out.println("read: "+new String(bs));
+                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);                
+                InputStream in = serialPort.getInputStream();                
+                while(true){
+                	
+					byte[] bs = new byte[1];
+					in.read(bs);
+					String st = new String(bs).trim();
+					if(st!=null&&!st.equals(""))
+					System.out.println("read: "+new String(bs));
 				
-				}
-                //(new Thread(new SerialReader(in))).start();
-                //(new Thread(new SerialWriter(out))).start();
-
+                }
             }
-            else
-            {
+            else {
                 System.out.println("Error: Only serial ports are handled by this example.");
             }
         }     
     }
     
-    /** */
-    public static class SerialReader implements Runnable 
-    {
+    /** class to read from serial stream */
+    public static class SerialReader implements Runnable {
         InputStream in;        
         public SerialReader ( InputStream in ){
             this.in = in;
         }        
         public void run (){
-			//System.out.println("Going to start reading");
             byte[] buffer = new byte[1024];
             int len = -1;
             try{
@@ -67,19 +77,14 @@ public class ButtonRead8051 {
     }
 
     /*
-    public static void main ( String[] args )
-    {
-        try
-        {
+    public static void main ( String[] args ){
+        try{
             (new ButtonRead8051()).connect("COM1");
         }
-        catch ( Exception e )
-        {
-            // TODO Auto-generated catch block
+        catch ( Exception e ){            
             e.printStackTrace();
         }
     }
     */
-
 
 }
