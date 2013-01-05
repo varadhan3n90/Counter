22adf89bf776cc9b379ddeef1e9b056cc66f7e18
Varadhan <varadhan3n90@yahoo.co.in>
2013-01-05 22:53:32 +0530

    made token.java serializable so that values of token dispensed can be stored in an object file.

--- Counter/src/server/Token.java
+++ Counter/src/server/Token.java
@@ -1,5 +1,18 @@
 package server;
 
-public class Token {
+import java.io.Serializable;
+
+public class Token implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	// TODO: Create fields for token
+	private int number;
+	public Token(int number){
+		this.number = number;
+	}
+	public int getTokenValue(){
+		return number;
+	}
 }
