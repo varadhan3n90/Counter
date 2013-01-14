b55272bcbdeef882fcb669305a5eb42a2e7f7c02
Varadhan <varadhan3n90@yahoo.co.in>
2013-01-14 22:47:18 +0530

    Changed store as it was not written to file after changes.

--- Counter/src/utility/ConfigurationReader.java
+++ Counter/src/utility/ConfigurationReader.java
@@ -1,38 +1,37 @@
 package utility;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
 	
-	private static final String SOURCE_TO_PROPERTY = System.getProperty("user.dir");
-	private static final String SERVER_INIT_CONFIG_FILE = "/Resources/serverstart.properties";
+	// private static final String SOURCE_TO_PROPERTY = System.getProperty("user.dir");
+	private static final String SERVER_INIT_CONFIG_FILE = "Resources/serverstart.properties";
 	private static final Properties SERVER_INIT_PROPERTIES = getPropertyFromFile(SERVER_INIT_CONFIG_FILE);
-	private static final String CLIENT_INIT_CONFIG_FILE = "/Resources/clientstart.properties";
+	private static final String CLIENT_INIT_CONFIG_FILE = "Resources/clientstart.properties";
 	private static final Properties CLIENT_INIT_PROPERTIES = getPropertyFromFile(CLIENT_INIT_CONFIG_FILE);
 	
 			
 			
-	public static Properties getPropertyFromFile(String fileName)
-	{
-		String absoluteFilePath;
-		absoluteFilePath = SOURCE_TO_PROPERTY + fileName;
+	private static Properties getPropertyFromFile(String fileName){
+		// String absoluteFilePath;
+		// absoluteFilePath = SOURCE_TO_PROPERTY + fileName;
 		try { 
 			Properties fileProperties = new Properties();
-			fileProperties.load(new FileInputStream(absoluteFilePath));
+			fileProperties.load(new FileInputStream(fileName));
 			return fileProperties;
 		}
-		catch (Exception err)
-		{
+		catch (Exception err){
 			System.out.println(err.getMessage());
-			System.out.println("Error opening the property file  " + absoluteFilePath);
-			
+			System.out.println("Error opening the property file  " + fileName);			
 		}
 		return null;
 			
 	}
 	
-   public static String getServerInit(String key)
-   {
+   public static String getServerInit(String key){
 	   String property = SERVER_INIT_PROPERTIES.getProperty(key);
 	   if ( property == null)
 		   return "";
@@ -40,8 +39,7 @@ public class ConfigurationReader {
 		   return property;
    }
    
-   public static String getClientInit(String key)
-   {
+   public static String getClientInit(String key){
 	   String property = CLIENT_INIT_PROPERTIES.getProperty(key);
 	   if ( property == null)
 		   return "";
@@ -49,14 +47,37 @@ public class ConfigurationReader {
 		   return property;
    }
    
-   public static void setClientInit(String key,String value)
-   {
+   public static void setClientInit(String key,String value){
 	  CLIENT_INIT_PROPERTIES.setProperty(key, value);
+	  try {
+		CLIENT_INIT_PROPERTIES.store(new FileOutputStream(CLIENT_INIT_CONFIG_FILE), null);
+	  } catch (FileNotFoundException e) {
+		System.out.println("Unable to find properties file");
+	  } catch (IOException e) {
+		System.out.println("Error writing to properties file");
+	  }
    }
    
-   public static void setServerInit(String key,String value)
-   {
+   public static void setServerInit(String key,String value){
 	   SERVER_INIT_PROPERTIES.setProperty(key, value);
+	   try {
+			SERVER_INIT_PROPERTIES.store(new FileOutputStream(SERVER_INIT_CONFIG_FILE), null);
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to find properties file");
+		} catch (IOException e) {
+			System.out.println("Error writing to properties file");
+		}
    }
 
+   /** 
+    * Test code
+    */
+   /*
+   public static void main(String[] args){
+	   String key = "server_ip";
+	   String value = "localhost";
+	   ConfigurationReader.setClientInit(key, value);
+	   System.out.println(ConfigurationReader.getClientInit("server_ip"));
+   }
+   */
 }
